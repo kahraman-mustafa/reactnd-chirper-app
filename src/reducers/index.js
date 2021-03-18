fe4187cdfa911da44c752dd8b7ff1697f1ac78b6
kahraman-mustafa <33468930+kahraman-mustafa@users.noreply.github.com>
2021-03-18 23:42:30 +0300

    Middlewares are added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
-import { users as usersReducer } from "../reducers/users.js";
-import { tweets as tweetsReducer } from "../reducers/tweets.js";
-import { authedUser as authedUserReducer } from "../reducers/authedUser.js"
+import users from "./users.js";
+import tweets from "./tweets.js";
+import authedUser from "./authedUser.js"
 
 export default combineReducers({
-    usersReducer,
-    tweetsReducer,
-    authedUserReducer
+    users,
+    tweets,
+    authedUser
 })
